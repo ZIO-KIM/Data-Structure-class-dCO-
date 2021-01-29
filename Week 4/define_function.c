e3de7c9d81ef90f891ab0bd2df63cecdd96233e7
Zio Kim <48899064+ZIO-KIM@users.noreply.github.com>
2021-01-29 22:16:23 +0900

    defining some functions (findMax, print_characters)

---
+++ Week 4/define_function.c
@@ -0,0 +1,19 @@
+int findMax(int a, int b) {
+
+	if (a > b) {
+		return a; 
+	}
+
+	else {
+		return b; 
+	}
+}
+
+void print_characters(char c, int n) {
+	
+	int i; 
+
+	for (i = 0; i < n; i++) {
+		print("%c", c); 
+	}
+}

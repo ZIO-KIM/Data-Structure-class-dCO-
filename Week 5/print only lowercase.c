f8fecd51b1e3093159ebe67c6db2b62e85ee325c
Zio Kim <48899064+ZIO-KIM@users.noreply.github.com>
2021-02-05 23:38:11 +0900

    5주차 실습 #2 - 소문자만 출력하기

---
+++ Week 5/print only lowercase.c
@@ -0,0 +1,22 @@
+#pragma warning(disable: 4996)
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main() {
+
+  char str[21]; 
+  int i; 
+
+  scanf("%s", str); 
+
+  for (i=0; i < strlen(str); i++)
+  {
+    if (str[i] >= 'a' && str[i] <= 'z')
+    {
+      printf("%c", str[i]); 
+    }
+  }
+
+	return 0; 
+}

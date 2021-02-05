6cf0d3d8ff8e17f30858ec5720bade93775ad7ea
Zio Kim <48899064+ZIO-KIM@users.noreply.github.com>
2021-02-05 21:18:12 +0900

    실습문제 #1

---
+++ Week 5/basic string.c
@@ -0,0 +1,17 @@
+#pragma warning(disable: 4996)
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main() {
+
+  char str[10] = "Hello"; 
+
+  printf("%s\n", str); 
+
+  scanf("%s", str); 
+
+  printf("%s", str); 
+
+	return 0; 
+}
